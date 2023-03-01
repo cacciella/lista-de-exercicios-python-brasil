de616c691ff4c208bd1b9d08749ffc26742b678d
Mauricio Cacciella <62062182+cacciella@users.noreply.github.com>
2023-03-01 16:35:27 -0300

    Update ex_02_escreva_um_numero.py

--- secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
+++ secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
@@ -14,5 +14,7 @@ Faça um Programa que peça um número e então mostre a mensagem O número info
 numero = int(input('Digite um número:  '))
 print(f'O número informado foi {numero}')
 
+"""
 Digite um número:  42
 O número informado foi 42
+"""
