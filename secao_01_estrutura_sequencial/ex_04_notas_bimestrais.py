44ce05cecd751355e5002602e03e45f0ca426c59
Mauricio Cacciella <62062182+cacciella@users.noreply.github.com>
2023-03-10 18:36:56 -0300

    Update ex_04_notas_bimestrais.py

--- secao_01_estrutura_sequencial/ex_04_notas_bimestrais.py
+++ secao_01_estrutura_sequencial/ex_04_notas_bimestrais.py
@@ -12,12 +12,12 @@ Faça um Programa que peça as 4 notas bimestrais e mostre a média.
 
 """
 
-
 def calcular_media():
     """Escreva aqui em baixo a sua solução"""
-notas_01 = int(input("Nota bimestral 01:   ")) 
-notas_02 = int(input("Nota bimestral 01:   ")) 
-notas_03 = int(input("Nota bimestral 01:   ")) 
-notas_04 = int(input("Nota bimestral 01:   ")) 
+
+notas_01 = int(input("Nota bimestral 01:   "))
+notas_02 = int(input("Nota bimestral 02:   "))
+notas_03 = int(input("Nota bimestral 03:   "))
+notas_04 = int(input("Nota bimestral 04:   "))
 media = ((notas_01 + notas_02 + notas_03 + notas_04) / 4)
-print(f."Suas notas bimestrais são: {notas_01}, {notas_02}, {notas_03} e {notas_04}" e "E sua media anual é: {media}")
+print(f"Suas notas bimestrais são: {notas_01}, {notas_02}, {notas_03} e {notas_04}" f" e a sua media anual é: {media}")
