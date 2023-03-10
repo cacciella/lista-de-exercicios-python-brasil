86f9a53d14d7f64e821e0ad4cbbbb444c27a3fe4
Mauricio Cacciella <62062182+cacciella@users.noreply.github.com>
2023-03-10 18:44:24 -0300

    Update ex_04_notas_bimestrais.py

--- secao_01_estrutura_sequencial/ex_04_notas_bimestrais.py
+++ secao_01_estrutura_sequencial/ex_04_notas_bimestrais.py
@@ -1,5 +1,5 @@
 """
-Exercício 03 da seção de estrutura sequencial da Python Brasil:
+Exercício 04 da seção de estrutura sequencial da Python Brasil:
 https://wiki.python.org.br/EstruturaSequencial
 
 Faça um Programa que peça as 4 notas bimestrais e mostre a média.
